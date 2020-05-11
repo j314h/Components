6968ab48e630a433b3721c356e63f7dd95421a09
j314h <johnhaimez314@gmail.com>
2020-05-11 11:37:45 +0200

    create object rose

--- ModalWindow/Src/Js/list_roses.js
+++ ModalWindow/Src/Js/list_roses.js
@@ -1,4 +1,4 @@
-const Rose = {
+const ROSE = {
     detail:"",
     infodivers = {
         denomination:"",
@@ -8,3 +8,13 @@ const Rose = {
     }
 }
 
+let BERNADETTE_LAFONT = new ROSE()
+BernadetteLafont.detail = "Rosier de petite taille aux fleurs très parfumées, très utilisées en fleurs coupées. Fleurs de 10cm de diamètres très riche en pétales. Port buissonnant."
+BernadetteLafont.infodivers.denomination = "Sauvachild"
+BernadetteLafont.infodivers.obtender = "Sauvageot"
+BernadetteLafont.infodivers.flowertype = "hybride de thé"
+BernadetteLafont.infodivers.resistancevirus = "Oui"
+
+
+
+export default BERNADETTE_LAFONT 
